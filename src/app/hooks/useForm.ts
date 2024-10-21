cd0c22c179070184c5cdfdb03390cc7485549c1a
yvonneannet <yvonneoyellaa@gmail.com>
2024-10-22 00:31:17 +0300

    run build

--- src/app/hooks/useForm.ts
+++ src/app/hooks/useForm.ts
@@ -1,21 +1,3 @@
-// import { useState } from 'react';
-
-// export function useForm(initialState = {}) {
-//   const [values, setValues] = useState(initialState);
-
-//   const handleChange = (event: { target: { name: any; value: any; }; }) => {
-//     const { name, value } = event.target;
-//     setValues(prevValues => ({ ...prevValues, [name]: value }));
-//   };
-
-//   const resetForm = () => {
-//     setValues(initialState);
-//   };
-
-//   return { values, handleChange, resetForm };
-// }
-
-
 
 
 import { useState, ChangeEvent } from 'react';
